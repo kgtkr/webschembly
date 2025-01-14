cebd74efd4803e8c9317700bf6cfce21181cca88
kgtkr <contact@kgtkr.net>
2025-01-15 03:04:49 +0900

    gitignore

--- e2e/test.js
+++ e2e/test.js
@@ -1,11 +1,19 @@
 // read ../target/wasm32-unknown-unknown/debug/webschembly_runtime.wasm
 const fs = require("fs");
-const buf = fs.readFileSync(
-  "../target/wasm32-unknown-unknown/debug/webschembly_runtime.wasm"
+const runtimeInstance = new WebAssembly.Instance(
+  new WebAssembly.Module(
+    new Uint8Array(
+      fs.readFileSync(
+        "../target/wasm32-unknown-unknown/debug/webschembly_runtime.wasm"
+      )
+    )
+  ),
+  {}
 );
-const wasm = new WebAssembly.Module(new Uint8Array(buf));
-const instance = new WebAssembly.Instance(wasm, {});
 
-console.log(instance.exports.memory);
-console.log(instance.exports.malloc(100));
-console.log(instance.exports.malloc(100));
+const instance = new WebAssembly.Instance(
+  new WebAssembly.Module(new Uint8Array(fs.readFileSync("./test.wasm"))),
+  {
+    runtime: runtimeInstance.exports,
+  }
+);
