4b258da4c15b804c577f384ee2e6d3b6b58e553a
kgtkr <contact@kgtkr.net>
2025-02-03 20:18:13 +0900

    runtime改善(read_stringがバグってる)

--- webschembly-runtime/src/lib.rs
+++ webschembly-runtime/src/lib.rs
@@ -1,19 +1,10 @@
-use core::{
-    cell::{Cell, UnsafeCell},
-    mem::MaybeUninit,
-};
+#![feature(ptr_as_ref_unchecked)]
+use core::cell::{Cell, RefCell, UnsafeCell};
 use std::collections::HashMap;
 
 const HEAP_SIZE: usize = 1024 * 1024;
 static mut HEAP: UnsafeCell<[u8; HEAP_SIZE]> = UnsafeCell::new([0; HEAP_SIZE]);
 static mut HEAP_OFFSET: Cell<usize> = Cell::new(0);
-
-static mut SYMBOL_INIT: Cell<bool> = Cell::new(false);
-static mut SYMBOL_ID: Cell<usize> = Cell::new(0);
-static mut SYMBOL_TO_BYTES: UnsafeCell<MaybeUninit<HashMap<usize, Vec<u8>>>> =
-    UnsafeCell::new(MaybeUninit::uninit());
-static mut BYTES_TO_SYMBOL: UnsafeCell<MaybeUninit<HashMap<Vec<u8>, usize>>> =
-    UnsafeCell::new(MaybeUninit::uninit());
 
 #[unsafe(no_mangle)]
 pub extern "C" fn malloc(size: i32) -> i32 {
@@ -30,44 +21,54 @@ pub extern "C" fn dump(value: i64) {
     println!("{:b}", value);
 }
 
+thread_local!(
+    static SYMBOL_MANAGER: RefCell<SymbolManager> = RefCell::new(SymbolManager::new());
+);
+
 #[unsafe(no_mangle)]
 pub extern "C" fn string_to_symbol(string: i32) -> i32 {
-    unsafe {
-        let symbol_init = SYMBOL_INIT.get();
-        if !symbol_init {
-            SYMBOL_TO_BYTES
-                .get()
-                .write(MaybeUninit::new(HashMap::new()));
-            BYTES_TO_SYMBOL
-                .get()
-                .write(MaybeUninit::new(HashMap::new()));
-            SYMBOL_INIT.set(true);
+    let string = unsafe { read_string(string) };
+    SYMBOL_MANAGER.with(|symbol_manager| symbol_manager.borrow_mut().string_to_symbol(string))
+}
+
+unsafe fn read_string(string: i32) -> Vec<u8> {
+    let len_ptr = string as *const i32;
+    let buf_ptr = (string + 4) as *const u8;
+
+    let len = *len_ptr;
+    let mut bytes = Vec::with_capacity(len as usize);
+    for i in 0..len {
+        bytes.push(*buf_ptr.offset(i as isize));
+    }
+
+    bytes
+}
+
+struct SymbolManager {
+    symbol_to_bytes: HashMap<usize, Vec<u8>>,
+    bytes_to_symbol: HashMap<Vec<u8>, usize>,
+    symbol_id: usize,
+}
+
+impl SymbolManager {
+    fn new() -> Self {
+        Self {
+            symbol_to_bytes: HashMap::new(),
+            bytes_to_symbol: HashMap::new(),
+            symbol_id: 0,
         }
+    }
 
-        let len = *(string as *const i32);
-        let mut bytes = Vec::with_capacity(len as usize);
-        for i in 0..len {
-            bytes.push(*((string + 4) as *const u8).offset(i as isize));
-        }
-
-        let symbol_to_bytes = SYMBOL_TO_BYTES.get().read().as_mut_ptr();
-        let bytes_to_symbol = BYTES_TO_SYMBOL.get().read().as_mut_ptr();
-
-        let symbol_id = SYMBOL_ID.get();
-
-        if let Some(symbol) = (*BYTES_TO_SYMBOL.get()).assume_init_ref().get(&bytes) {
+    fn string_to_symbol(&mut self, string: Vec<u8>) -> i32 {
+        if let Some(symbol) = self.bytes_to_symbol.get(&string) {
             return *symbol as i32;
         }
 
-        (*SYMBOL_TO_BYTES.get())
-            .assume_init_mut()
-            .insert(symbol_id, bytes.clone());
-        (*BYTES_TO_SYMBOL.get())
-            .assume_init_mut()
-            .insert(bytes, symbol_id);
+        self.symbol_to_bytes.insert(self.symbol_id, string.clone());
+        self.bytes_to_symbol.insert(string, self.symbol_id);
 
-        SYMBOL_ID.set(symbol_id + 1);
+        self.symbol_id += 1;
 
-        symbol_id as i32
+        (self.symbol_id - 1) as i32
     }
 }
