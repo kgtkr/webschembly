adf1019740b257cd8c103aa7cb3ed90e7563c242
kgtkr <me@kgtkr.net>
2025-02-26 01:52:03 +0900

    webschembly-runtimeのビルド問題修正

--- webschembly-runtime/default.nix
+++ webschembly-runtime/default.nix
@@ -1,10 +1,10 @@
-{ stdenv, binaryen, webschembly-runtime-rust, gnumake }:
+{ stdenv, binaryen, webschembly-runtime-rust, gnumake, BINARYEN_ARGS }:
 stdenv.mkDerivation {
   name = "webschembly-runtime";
   src = ./.;
   buildInputs = [ binaryen webschembly-runtime-rust gnumake ];
   buildPhase = ''
-    make WEBSCHEMBLY_RUNTIME_RUST=${webschembly-runtime-rust}/lib/webschembly_runtime_rust.wasm
+    make WEBSCHEMBLY_RUNTIME_RUST=${webschembly-runtime-rust}/lib/webschembly_runtime_rust.wasm BINARYEN_ARGS="${BINARYEN_ARGS}"
   '';
   installPhase = ''
     mkdir -p $out/lib
