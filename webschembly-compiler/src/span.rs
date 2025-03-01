02e9f4bdc37c1b707a7daccdad29ab300183e0a5
kgtkr <me@kgtkr.net>
2025-03-02 03:47:29 +0900

    format span

--- webschembly-compiler/src/span.rs
+++ webschembly-compiler/src/span.rs
@@ -1,3 +1,23 @@
+use std::fmt::Display;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Pos {
+    pub line: usize,
+    pub column: usize,
+}
+
+impl Pos {
+    pub fn new(line: usize, column: usize) -> Self {
+        Self { line, column }
+    }
+}
+
+impl Display for Pos {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:{}", self.line, self.column)
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct Span {
     pub start: Pos,
@@ -17,14 +37,8 @@ impl Span {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-pub struct Pos {
-    pub line: usize,
-    pub column: usize,
-}
-
-impl Pos {
-    pub fn new(line: usize, column: usize) -> Self {
-        Self { line, column }
+impl Display for Span {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}-{}", self.start, self.end)
     }
 }
