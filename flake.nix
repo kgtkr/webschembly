c11fd69efdaeb84f3c94947af8b31491179527f6
kgtkr <me@kgtkr.net>
2025-03-06 19:58:49 +0900

    ソースのフィルターを強化

--- flake.nix
+++ flake.nix
@@ -21,18 +21,26 @@
           inherit system;
           overlays = [ cargo2nix.overlays.default napalm.overlays.default ];
         };
+        inherit (pkgs) lib stdenv;
         nodejs = pkgs.nodejs_22;
         rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         projectName = "webschembly";
         staticTarget = pkgs.pkgsStatic.stdenv.hostPlatform.config;
+        mkSrc = srcs: stdenv.mkDerivation {
+          name = "src";
+          phases = [ "installPhase" ];
+          installPhase = ''
+            mkdir -p $out
+            ${lib.concatStringsSep "\n" (lib.map (name: "cp -r ${lib.path.append ./. name} $out/${name}") srcs)}
+          '';
+        };
         filteredSrc = cargo2nix-ifd.lib.${system}.filterSrc {
-          src = ./.;
-          orFilter = orig_path: type:
+          src = mkSrc (["Cargo.toml" "Cargo.lock"] ++ (fromTOML (builtins.readFile ./Cargo.toml)).workspace.members);
+          orFilter = path: _type:
             let
-              path = (toString orig_path);
-              base = baseNameOf path;
+              files = ["stdlib.scm"];
             in
-            base == "stdlib.scm";
+              lib.any (file: baseNameOf path == file) files;
           inherit projectName;
         };
         generatedSrc = cargo2nix-ifd.lib.${system}.generateSrc {
@@ -57,12 +65,12 @@
         };
         webschembly-compiler-cli = (staticRustPkgs.workspace.webschembly-compiler-cli { }).bin;
         webschembly-runtime-rust = (wasmRustPkgs.workspace.webschembly-runtime-rust { }).out;
-        webschembly-runtime = pkgs.callPackage ./webschembly-runtime { inherit webschembly-runtime-rust; BINARYEN_ARGS = pkgs.lib.strings.trim (builtins.readFile ./binaryen-args.txt); };
-        webschembly-node_modules = pkgs.napalm.buildPackage ./. {
+        webschembly-runtime = pkgs.callPackage ./webschembly-runtime { inherit webschembly-runtime-rust; BINARYEN_ARGS = lib.strings.trim (builtins.readFile ./binaryen-args.txt); };
+        webschembly-node_modules = pkgs.napalm.buildPackage (mkSrc (["package.json" "package-lock.json"] ++ (builtins.fromJSON (builtins.readFile ./package.json)).workspaces)) {
           inherit nodejs;
           name = "webschembly-node_modules";
         };
-        webschembly-playground = pkgs.stdenv.mkDerivation {
+        webschembly-playground = stdenv.mkDerivation {
           name = "webschembly-playground";
           buildInputs = [ pkgs.gnumake nodejs ];
           src = "${webschembly-node_modules}/_napalm-install";
