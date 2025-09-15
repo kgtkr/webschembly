e6ad3a2523f359d2cf609645386569de62f3e431
github-action-benchmark <github@users.noreply.github.com>
2025-09-15 09:33:39 +0000

    add Benchmark (benchmarkjs) benchmark result for 43acfa1f16b4dd3d5fb636c7a2b3d7331e738328

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1757928819885,
+  "repoUrl": "https://github.com/kgtkr/webschembly",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "me@kgtkr.net",
+            "name": "kgtkr",
+            "username": "kgtkr"
+          },
+          "committer": {
+            "email": "me@kgtkr.net",
+            "name": "kgtkr",
+            "username": "kgtkr"
+          },
+          "distinct": true,
+          "id": "43acfa1f16b4dd3d5fb636c7a2b3d7331e738328",
+          "message": "fix permissions",
+          "timestamp": "2025-09-15T18:31:42+09:00",
+          "tree_id": "885785847c78130b07b8576ab18b7aaa1d5900d4",
+          "url": "https://github.com/kgtkr/webschembly/commit/43acfa1f16b4dd3d5fb636c7a2b3d7331e738328"
+        },
+        "date": 1757928818925,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "div2.b.scm",
+            "value": 3.57,
+            "range": "±3.10%",
+            "unit": "ops/sec",
+            "extra": "13 samples"
+          },
+          {
+            "name": "tak.b.scm",
+            "value": 20.02,
+            "range": "±1.69%",
+            "unit": "ops/sec",
+            "extra": "37 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
