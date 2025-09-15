a2bca76be55cff36cac881aca16b2a7f88118747
kgtkr <me@kgtkr.net>
2025-09-15 17:32:54 +0900

    add Benchmark

---
+++ webschembly-js/src/e2e.benchmark.ts
@@ -0,0 +1,42 @@
+import Benchmark from "benchmark";
+
+import * as fs from "fs/promises";
+import * as path from "path";
+import { createRuntime } from "./runtime";
+import { createNodeRuntimeEnv } from "./node-runtime-env";
+
+const sourceDir = "fixtures";
+const filenames = (await fs.readdir(sourceDir)).filter((file) =>
+  file.endsWith(".b.scm")
+);
+
+const runtimeModule = new WebAssembly.Module(
+  await fs.readFile(process.env["WEBSCHEMBLY_RUNTIME"]!)
+);
+
+const suite = new Benchmark.Suite();
+
+for (const filename of filenames) {
+  const srcBuf = await fs.readFile(path.join(sourceDir, filename));
+  const runtime = await createRuntime(
+    await createNodeRuntimeEnv({
+      runtimeName: filename,
+      exit: () => {},
+      writeBuf: () => {},
+      loadRuntimeModule: async () => runtimeModule,
+    }),
+    {}
+  );
+
+  suite.add(filename, () => {
+    runtime.loadStdlib();
+    runtime.loadSrc(srcBuf);
+    runtime.cleanup();
+  });
+}
+
+suite
+  .on("cycle", (event: any) => {
+    console.log(String(event.target));
+  })
+  .run();
