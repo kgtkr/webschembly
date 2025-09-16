79755b6d807dc4374919e3755c9fa923000dc921
kgtkr <me@kgtkr.net>
2025-09-17 01:41:09 +0900

    jit処理分割

--- webschembly-compiler/src/ir_generator/jit.rs
+++ webschembly-compiler/src/ir_generator/jit.rs
@@ -7,14 +7,14 @@ use crate::ir_generator::GlobalManager;
 #[derive(Debug)]
 pub struct Jit {
     modules: TiVec<ModuleId, Module>,
-    module_managers: TiVec<ModuleId, JitModuleManager>,
+    jit_module: TiVec<ModuleId, JitModule>,
 }
 
 impl Jit {
     pub fn new() -> Self {
         Self {
             modules: TiVec::new(),
-            module_managers: TiVec::new(),
+            jit_module: TiVec::new(),
         }
     }
 
@@ -24,25 +24,28 @@ impl Jit {
         module: Module,
     ) -> Module {
         let module_id = self.modules.push_and_get_key(module);
-        self.module_managers
-            .push(JitModuleManager::new(global_manager, self, module_id));
-        self.module_managers[module_id].generate_stub_module(self)
+        self.jit_module
+            .push(JitModule::new(global_manager, self, module_id));
+        self.jit_module[module_id].generate_stub_module(self)
     }
 
     pub fn instantiate_func(&self, module_id: ModuleId, func_id: FuncId) -> Module {
-        self.module_managers[module_id].generate_func_module(self, func_id)
+        let jit_module = &self.jit_module[module_id];
+        jit_module
+            .generate_jit_func(self, func_id)
+            .generate_stub_module(self, jit_module)
     }
 }
 
 #[derive(Debug)]
-pub struct JitModuleManager {
+struct JitModule {
     module_id: ModuleId,
     func_ref_globals: TiVec<FuncId, GlobalId>,
     globals: FxHashSet<GlobalId>,
 }
 
-impl JitModuleManager {
-    pub fn new(global_manager: &mut GlobalManager, jit: &Jit, module_id: ModuleId) -> Self {
+impl JitModule {
+    fn new(global_manager: &mut GlobalManager, jit: &Jit, module_id: ModuleId) -> Self {
         let module = &jit.modules[module_id];
         let func_ref_globals = module
             .funcs
@@ -63,7 +66,7 @@ impl JitModuleManager {
         }
     }
 
-    pub fn generate_stub_module(&self, jit: &Jit) -> Module {
+    fn generate_stub_module(&self, jit: &Jit) -> Module {
         let module = &jit.modules[self.module_id];
         // entry関数もあるので+1してる
         let stub_func_ids = module
@@ -222,8 +225,52 @@ impl JitModuleManager {
         }
     }
 
-    pub fn generate_func_module(&self, jit: &Jit, func_id: FuncId) -> Module {
-        let module = &jit.modules[self.module_id];
+    fn generate_jit_func(&self, jit: &Jit, func_id: FuncId) -> JitFunc {
+        JitFunc::new(&mut GlobalManager::new(), jit, self, func_id)
+    }
+}
+
+#[derive(Debug)]
+struct JitFunc {
+    func_id: FuncId,
+    func_ref_globals: TiVec<BasicBlockId, GlobalId>,
+    bb_infos: TiVec<BasicBlockId, BBInfo>,
+    globals: FxHashSet<GlobalId>,
+}
+
+impl JitFunc {
+    fn new(
+        global_manager: &mut GlobalManager,
+        jit: &Jit,
+        jit_module: &JitModule,
+        func_id: FuncId,
+    ) -> Self {
+        let module = &jit.modules[jit_module.module_id];
+        let func = &module.funcs[func_id];
+        let func_ref_globals = func
+            .bbs
+            .iter()
+            .map(|_| global_manager.gen_global_id())
+            .collect::<TiVec<BasicBlockId, _>>();
+
+        let bb_infos = calculate_bb_info(analyze_locals(func));
+
+        let globals = {
+            let mut globals = jit_module.globals.clone();
+            globals.extend(func_ref_globals.iter());
+            globals
+        };
+
+        Self {
+            func_id,
+            func_ref_globals,
+            bb_infos,
+            globals,
+        }
+    }
+
+    fn generate_stub_module(&self, jit: &Jit, jit_module: &JitModule) -> Module {
+        let module = &jit.modules[jit_module.module_id];
         /*
         以下に対応するモジュールを生成
         func entry() {
@@ -236,7 +283,7 @@ impl JitModuleManager {
         }
 
         */
-        let func = &module.funcs[func_id];
+        let func = &module.funcs[self.func_id];
 
         let mut funcs = TiVec::<FuncId, _>::new();
         let entry_func = Func {
@@ -265,7 +312,10 @@ impl JitModuleManager {
                     },
                     ExprAssign {
                         local: None,
-                        expr: Expr::GlobalSet(self.func_ref_globals[func.id], LocalId::from(1)),
+                        expr: Expr::GlobalSet(
+                            jit_module.func_ref_globals[func.id],
+                            LocalId::from(1)
+                        ),
                     }
                 ],
                 next: BasicBlockNext::Return(LocalId::from(0)),
@@ -297,7 +347,7 @@ impl JitModuleManager {
                             Expr::FuncRef(id) => {
                                 exprs.push(ExprAssign {
                                     local: Some(boxed_func_ref),
-                                    expr: Expr::GlobalGet(self.func_ref_globals[*id]),
+                                    expr: Expr::GlobalGet(jit_module.func_ref_globals[*id]),
                                 });
                                 exprs.push(ExprAssign {
                                     local: expr.local,
@@ -307,7 +357,7 @@ impl JitModuleManager {
                             Expr::Call(ExprCall { func_id, args }) => {
                                 exprs.push(ExprAssign {
                                     local: Some(boxed_func_ref),
-                                    expr: Expr::GlobalGet(self.func_ref_globals[*func_id]),
+                                    expr: Expr::GlobalGet(jit_module.func_ref_globals[*func_id]),
                                 });
                                 exprs.push(ExprAssign {
                                     local: Some(func_ref),
@@ -332,7 +382,7 @@ impl JitModuleManager {
                         BasicBlockNext::TailCall(ExprCall { func_id, args }) => {
                             exprs.push(ExprAssign {
                                 local: Some(boxed_func_ref),
-                                expr: Expr::GlobalGet(self.func_ref_globals[*func_id]),
+                                expr: Expr::GlobalGet(jit_module.func_ref_globals[*func_id]),
                             });
                             exprs.push(ExprAssign {
                                 local: Some(func_ref),
@@ -373,4 +423,243 @@ impl JitModuleManager {
             },
         }
     }
+
+    /*  fn generate_bb_module(&self, jit: &Jit, bb_id: BasicBlockId) -> Module {
+        let module = &jit.modules[self.module_id];
+        let func = &module.funcs[self.func_id];
+        let bb = &func.bbs[bb_id];
+        let bb_info = &self.bb_infos[bb_id];
+
+        let mut new_locals = TiVec::new();
+
+        for &arg in &bb_info.args {
+            new_locals.push(func.locals[arg]);
+        }
+
+        for &define in &bb_info.defines {
+            new_locals.push(func.locals[define]);
+        }
+
+        for (local_id, _) in bb.local_usages_mut() {
+            *local_id = bb_info.locals_mapping[local_id];
+        }
+
+        for func_id in bb.func_ids_mut() {
+            let new_target_func_id = new_func_ids[func_id];
+            *func_id = new_target_func_id;
+        }
+        let mut extra_bbs = Vec::new();
+
+        let new_next = match bb.next {
+            BasicBlockNext::If(cond, then_bb, else_bb) => {
+                let then_func_id = bb_to_func_id[&(orig_func.id, then_bb)];
+                let else_func_id = bb_to_func_id[&(orig_func.id, else_bb)];
+
+                let then_locals_to_pass =
+                    calculate_args_to_pass(bb_info, &bb_infos[orig_func.id][then_bb]);
+                let else_locals_to_pass =
+                    calculate_args_to_pass(bb_info, &bb_infos[orig_func.id][else_bb]);
+
+                let then_bb_new = BasicBlock {
+                    id: BasicBlockId::from(1),
+                    exprs: vec![],
+                    next: BasicBlockNext::TailCall(ExprCall {
+                        func_id: then_func_id,
+                        args: then_locals_to_pass,
+                    }),
+                };
+
+                let else_bb_new = BasicBlock {
+                    id: BasicBlockId::from(2),
+                    exprs: vec![],
+                    next: BasicBlockNext::TailCall(ExprCall {
+                        func_id: else_func_id,
+                        args: else_locals_to_pass,
+                    }),
+                };
+
+                extra_bbs.push(then_bb_new);
+                extra_bbs.push(else_bb_new);
+
+                BasicBlockNext::If(cond, BasicBlockId::from(1), BasicBlockId::from(2))
+            }
+            BasicBlockNext::Jump(target_bb) => {
+                let target_func_id = bb_to_func_id[&(orig_func.id, target_bb)];
+                let args_to_pass =
+                    calculate_args_to_pass(bb_info, &bb_infos[orig_func.id][target_bb]);
+
+                BasicBlockNext::TailCall(ExprCall {
+                    func_id: target_func_id,
+                    args: args_to_pass,
+                })
+            }
+            next @ (BasicBlockNext::Return(_)
+            | BasicBlockNext::TailCall(_)
+            | BasicBlockNext::TailCallRef(_)) => next,
+        };
+
+        let new_bb = BasicBlock {
+            id: BasicBlockId::from(0),
+            exprs: bb.exprs,
+            next: new_next,
+        };
+
+        let mut new_bbs = TiVec::new();
+        new_bbs.push(new_bb);
+        new_bbs.extend(extra_bbs.into_iter());
+
+        let new_func = Func {
+            id: new_func_id,
+            locals: new_locals,
+            args: bb_info.args.len(),
+            ret_type: orig_func.ret_type,
+            bb_entry: BasicBlockId::from(0),
+            bbs: new_bbs,
+            jit_strategy: if orig_func.bb_entry == bb.id {
+                orig_func.jit_strategy
+            } else {
+                FuncJitStrategy::BasicBlock
+            },
+        };
+
+        new_funcs.push(new_func);
+    }*/
+}
+
+#[derive(Debug, Clone, Default)]
+struct AnalyzeResult {
+    defined_locals: FxHashSet<LocalId>,
+    used_locals: FxHashSet<LocalId>,
+}
+
+fn analyze_locals(func: &Func) -> TiVec<BasicBlockId, AnalyzeResult> {
+    let mut results = TiVec::new();
+
+    for bb in func.bbs.iter() {
+        let mut defined = FxHashSet::default();
+        let mut used = FxHashSet::default();
+
+        if bb.id == func.bb_entry {
+            for i in 0..func.args {
+                defined.insert(LocalId::from(i));
+            }
+        }
+
+        for (local_id, flag) in bb.local_usages() {
+            match flag {
+                LocalFlag::Defined => {
+                    defined.insert(*local_id);
+                }
+                LocalFlag::Used => {
+                    used.insert(*local_id);
+                }
+            }
+        }
+
+        results.push(AnalyzeResult {
+            defined_locals: defined,
+            used_locals: used,
+        });
+    }
+
+    // BBは前方ジャンプがないことを仮定している
+    // 複雑な制御フローを持つ場合はトポロジカルソートなどが必要
+    let bb_ids = func.bbs.iter().map(|bb| bb.id).collect::<Vec<_>>();
+
+    // defineの集計は前から行う
+    // 自分より前のブロックで定義済みの関数
+    let mut prev_defines = TiVec::new();
+    for _ in bb_ids.iter() {
+        prev_defines.push(FxHashSet::<LocalId>::default());
+    }
+    for &bb_id in bb_ids.iter() {
+        let result = &mut results[bb_id];
+        for prev_define in prev_defines[bb_id].iter() {
+            let removed = result.defined_locals.remove(prev_define);
+            if removed {
+                result.used_locals.insert(*prev_define);
+            }
+        }
+
+        let prev_define = prev_defines[bb_id].clone();
+        for succ in func.bbs[bb_id].next.successors() {
+            prev_defines[succ].extend(&result.defined_locals);
+            prev_defines[succ].extend(&prev_define);
+        }
+    }
+
+    for &bb_id in bb_ids.iter().rev() {
+        let mut result = results[bb_id].clone();
+
+        for succ in func.bbs[bb_id].next.successors() {
+            let succ_result = &results[succ];
+            result.used_locals.extend(&succ_result.used_locals);
+        }
+
+        for define in &result.defined_locals {
+            result.used_locals.remove(define);
+        }
+
+        results[bb_id] = result;
+    }
+
+    // エントリーポイントの例外的処理
+    results[func.bb_entry].used_locals =
+        (0..func.args).map(LocalId::from).collect::<FxHashSet<_>>();
+    for i in 0..func.args {
+        results[func.bb_entry]
+            .defined_locals
+            .remove(&LocalId::from(i));
+    }
+
+    results
+}
+
+#[derive(Debug, Clone, Default)]
+struct BBInfo {
+    // argsとdefinesはorig func_id
+    args: Vec<LocalId>,
+    defines: Vec<LocalId>,
+    // orig func_id -> new func_id
+    locals_mapping: FxHashMap<LocalId, LocalId>,
+}
+
+fn calculate_bb_info(
+    analyze_results: TiVec<BasicBlockId, AnalyzeResult>,
+) -> TiVec<BasicBlockId, BBInfo> {
+    let mut bb_info = TiVec::new();
+
+    for result in analyze_results.into_iter() {
+        let mut args = result.used_locals.into_iter().collect::<Vec<_>>();
+        args.sort();
+        let mut defines = result.defined_locals.into_iter().collect::<Vec<_>>();
+        defines.sort();
+        let mut info = BBInfo {
+            args,
+            defines,
+            locals_mapping: FxHashMap::default(),
+        };
+
+        let mut local_id = LocalId::from(0);
+        for &arg in &info.args {
+            info.locals_mapping.insert(arg, local_id);
+            local_id = LocalId::from(usize::from(local_id) + 1);
+        }
+        for &define in &info.defines {
+            info.locals_mapping.insert(define, local_id);
+            local_id = LocalId::from(usize::from(local_id) + 1);
+        }
+
+        bb_info.push(info);
+    }
+
+    bb_info
+}
+
+fn calculate_args_to_pass(caller: &BBInfo, callee: &BBInfo) -> Vec<LocalId> {
+    let mut args_to_pass = Vec::new();
+    for &arg in &callee.args {
+        args_to_pass.push(caller.locals_mapping[&arg]);
+    }
+    args_to_pass
 }
