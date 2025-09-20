4ec7cfdaa1bdc8c5c9bf0b4ba50a3fa5d8cf5deb
kgtkr <me@kgtkr.net>
2025-09-20 21:06:24 +0900

    ベンチマークを毎回リセットするようにした

--- webschembly-js/src/e2e.benchmark.ts
+++ webschembly-js/src/e2e.benchmark.ts
@@ -6,6 +6,7 @@ import {
   compilerConfigToString,
   createRuntime,
   type CompilerConfig,
+  type Runtime,
 } from "./runtime";
 import { createNodeRuntimeEnv } from "./node-runtime-env";
 
@@ -23,28 +24,36 @@ const runtimeModule = new WebAssembly.Module(
   await fs.readFile(process.env["WEBSCHEMBLY_RUNTIME"]!)
 );
 
-const bench = new Bench({ iterations: 2 });
+const bench = new Bench();
 
 for (const compilerConfig of compilerConfigs) {
   for (const filename of filenames) {
     const srcBuf = await fs.readFile(path.join(sourceDir, filename));
-    const runtime = await createRuntime(
-      await createNodeRuntimeEnv({
-        runtimeName: filename,
-        exit: () => {},
-        writeBuf: () => {},
-        loadRuntimeModule: async () => runtimeModule,
-      }),
+    let runtime: Runtime;
+
+    bench.add(
+      `${filename},${compilerConfigToString(compilerConfig)}`,
+      () => {
+        runtime.loadStdlib();
+        runtime.loadSrc(srcBuf);
+        runtime.cleanup();
+      },
       {
-        compilerConfig,
+        beforeEach: async () => {
+          runtime = await createRuntime(
+            await createNodeRuntimeEnv({
+              runtimeName: filename,
+              exit: () => {},
+              writeBuf: () => {},
+              loadRuntimeModule: async () => runtimeModule,
+            }),
+            {
+              compilerConfig,
+            }
+          );
+        },
       }
     );
-
-    bench.add(`${filename},${compilerConfigToString(compilerConfig)}`, () => {
-      runtime.loadStdlib();
-      runtime.loadSrc(srcBuf);
-      runtime.cleanup();
-    });
   }
 }
 
