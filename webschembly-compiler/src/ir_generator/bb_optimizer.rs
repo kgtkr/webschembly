b0896ccdec2381da48c4035ba90693d7cb85db62
kgtkr <me@kgtkr.net>
2025-09-23 19:08:03 +0900

    テスト修正

--- webschembly-compiler/src/ir_generator/bb_optimizer.rs
+++ webschembly-compiler/src/ir_generator/bb_optimizer.rs
@@ -136,117 +136,6 @@ pub fn analyze_typed_box(
     typed_boxes
 }
 
-/*
-```
-local l1: boxed
-local l2: int
-local l3: int
-
-args: l1
-
-_ = cons(l1, l1)
-l2 = unbox<int>(l1)
-l3 = unbox<int>(l1)
-_ = add(l2, l3)
-```
-
-`l1 = int` を代入した場合:
-
-```
-local l1: int
-local l2: int
-local l3: int
-local l1_boxed: boxed // boxedバージョンを追加
-
-args: l1
-
-l1_boxed = boxed<int>(l1) // BBの先頭に追加。不要なら後々の最適化で削除する
-_ = cons(l1_boxed, l1_boxed) // l1を参照している式はl1_boxedに置き換える
-l2 = unbox<int>(l1_boxed)
-l3 = unbox<int>(l1_boxed)
-_ = add(l1, l1)
-```
-
-次のBBに引き継ぐ情報:
-- boxedな値→unboxedな値の対応とその型(どちらも再代入されない場合のみ)
-*/
-
-pub fn remove_box(
-    locals: &mut TiVec<LocalId, LocalType>,
-    bb: &mut BasicBlock,
-    type_params: &FxBiHashMap<TypeParamId, LocalId>,
-    type_args: &TiVec<TypeParamId, Option<ValType>>,
-    locals_immutability: &mut TiVec<LocalId, bool>,
-) -> TiVec<LocalId, Option<TypedBox>> {
-    let mut additional_expr_assigns = Vec::new();
-
-    // 型代入されている変数のboxed版を用意(l1_boxedに対応)
-    let mut boxed_locals = ti_vec![None; locals.len()];
-    for (type_param_id, typ) in type_args.iter_enumerated() {
-        if let Some(typ) = typ {
-            let boxed_local = locals.push_and_get_key(LocalType::Type(Type::Boxed));
-            boxed_locals[*type_params.get_by_left(&type_param_id).unwrap()] = Some(boxed_local);
-
-            additional_expr_assigns.push(ExprAssign {
-                local: Some(boxed_local),
-                expr: Expr::Box(*typ, *type_params.get_by_left(&type_param_id).unwrap()),
-            });
-            locals_immutability.push(true); // boxed_localは再代入されない
-        }
-    }
-
-    let boxed_locals = boxed_locals;
-
-    // ローカル変数の置き換え情報
-    let mut local_replacements = TiVec::new();
-    for (from, to) in boxed_locals.iter_enumerated() {
-        local_replacements.push(to.unwrap_or(from));
-    }
-
-    // 次のBBに引き継ぐ型情報
-    let mut next_type_args = ti_vec![None; locals.len()];
-
-    for expr_assign in bb.exprs.iter_mut() {
-        if let ExprAssign {
-            local,
-            expr: Expr::Unbox(typ, value),
-        } = *expr_assign
-            && locals_immutability[value]
-            && let Some(local) = local
-            && locals_immutability[local]
-        {
-            next_type_args[value] = Some(TypedBox {
-                unboxed: local,
-                typ,
-            });
-        }
-
-        for (local, _) in expr_assign.local_usages_mut() {
-            *local = local_replacements[*local];
-        }
-    }
-
-    for local in bb.next.local_ids_mut() {
-        *local = local_replacements[*local]
-    }
-
-    for (unboxed, &boxed) in boxed_locals.iter_enumerated() {
-        if let Some(boxed) = boxed
-            && locals_immutability[unboxed]
-            && locals_immutability[boxed]
-        {
-            let LocalType::Type(Type::Val(typ)) = locals[unboxed] else {
-                unreachable!("expect val type, actual: {:?}", locals[unboxed]);
-            };
-            next_type_args[boxed] = Some(TypedBox { unboxed, typ });
-        }
-    }
-
-    bb.exprs.splice(0..0, additional_expr_assigns);
-
-    next_type_args
-}
-
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct TypedBox {
     pub unboxed: LocalId,
@@ -449,6 +338,7 @@ mod tests {
         assert_eq!(immutability, ti_vec![false, true, false, true]);
     }
 
+    /*
     fn create_test_basic_block() -> BasicBlock {
         BasicBlock {
             id: BasicBlockId::from(0),
@@ -464,6 +354,7 @@ mod tests {
             LocalType::Type(Type::Val(ValType::Bool)), // l2
         ]
     }
+
 
     #[test]
     fn test_remove_box_with_empty_type_args() {
@@ -484,10 +375,11 @@ mod tests {
         assert_eq!(next_type_args.len(), 3);
         assert!(next_type_args.iter().all(|x| x.is_none()));
         assert_eq!(bb.exprs.len(), 0);
-    }
+    }*/
 
+    // TODO: assign_type_argsとanalyze_typed_boxのテストは分割したほうがいいかも？
     #[test]
-    fn test_remove_box() {
+    fn test_assign_type_args_and_analyze_typed_box() {
         let mut locals = ti_vec![
             LocalType::Type(Type::Boxed),
             LocalType::Type(Type::Val(ValType::Int)),
@@ -521,10 +413,7 @@ mod tests {
         let type_args = ti_vec![Some(ValType::Int)];
         let mut locals_immutability = ti_vec![true, true, true];
 
-        // type argsに対応するlocalsは事前に置き換えて渡すことが想定されている
-        locals[LocalId::from(0)] = LocalType::Type(Type::Val(ValType::Int));
-
-        let next_type_args = remove_box(
+        let assigned_local_to_box = assign_type_args(
             &mut locals,
             &mut bb,
             &type_params,
@@ -563,21 +452,27 @@ mod tests {
         ]);
 
         assert_eq!(locals_immutability, ti_vec![true, true, true, true]);
-        assert_eq!(next_type_args, ti_vec![
-            // TODO: ここがNoneにならないのAPI設計として汚い
+        assert_eq!(assigned_local_to_box, ti_vec![
+            Some(LocalId::from(3)),
+            None,
+            None,
+        ]);
+
+        let typed_boxes: TiVec<LocalId, Option<TypedBox>> =
+            analyze_typed_box(&locals, &bb, &locals_immutability);
+
+        assert_eq!(typed_boxes, ti_vec![
+            None,
+            None,
+            None,
             Some(TypedBox {
                 unboxed: LocalId::from(2),
-                typ: ValType::Int
-            }),
-            None,
-            None,
-            Some(TypedBox {
-                unboxed: LocalId::from(0),
                 typ: ValType::Int
             })
         ]);
     }
 
+    /*
     #[test]
     fn test_remove_box_with_unbox_expr() {
         let mut locals = ti_vec![
@@ -614,7 +509,7 @@ mod tests {
                 typ: ValType::Int,
             })
         );
-    }
+    }*/
 
     #[test]
     fn test_copy_propagate_move() {
