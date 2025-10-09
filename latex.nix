a2626eb3c15a4ddab9aeacb0ee9b61da032f8d02
kgtkr <me@kgtkr.net>
2025-10-09 23:59:03 +0900

    latexを小さくした

--- latex.nix
+++ latex.nix
@@ -1,10 +1,36 @@
-{ lib, ... }: {
+{ ... }: {
   perSystem = { self', pkgs, ... }:
+    let
+      tex = pkgs.texlive.combine {
+        inherit (pkgs.texlive) scheme-small
+          luatexbase
+          luatexja
+          lualatex-math
+          #tikz
+          pgf
+          #font
+          haranoaji
+          ;
+      };
+    in
     {
-      packages = { };
+      packages = {
+        webschembly-docs = pkgs.stdenv.mkDerivation {
+          name = "webschembly-docs";
+          src = ./docs;
+          buildInputs = [ tex pkgs.pandoc pkgs.gnumake ];
+          buildPhase = ''
+            make index.pdf
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp index.pdf $out/
+          '';
+        };
+      };
       make-shells.default = {
         packages = [
-          pkgs.texlive.combined.scheme-full
+          tex
           pkgs.pandoc
         ];
       };
