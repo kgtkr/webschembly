f0ed86cc7ad9734abfbddf630cbd4c0ee4e1a774
kgtkr <me@kgtkr.net>
2025-10-20 22:22:20 +0900

    IndentLevel

--- webschembly-compiler-crates/ir/src/display.rs
+++ webschembly-compiler-crates/ir/src/display.rs
@@ -1,3 +1,5 @@
+use derive_more::{From, Into};
+
 use super::meta::*;
 
 #[derive(Debug, Clone)]
@@ -13,3 +15,22 @@ pub struct DisplayInFunc<'a, T> {
 }
 
 pub const DISPLAY_INDENT: &str = "  ";
+
+#[derive(Debug, Clone, Copy, Default, Into, From)]
+pub struct IndentLevel(pub usize);
+
+impl IndentLevel {
+    pub fn indent(&self) -> String {
+        DISPLAY_INDENT.repeat(self.0)
+    }
+
+    pub fn increase(&self) -> Self {
+        IndentLevel(self.0 + 1)
+    }
+}
+
+impl std::fmt::Display for IndentLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.indent())
+    }
+}
